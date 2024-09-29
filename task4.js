daac5f3e445c15dede4f479c611793ebf4f15c3b
Vitaliy Maksymchuk <vitalii.v.maksymchuk@gmail.com>
2024-09-29 19:54:55 +0300

    task_4_completed

--- task4.js
+++ task4.js
@@ -3,6 +3,14 @@
 //   у якому перше входження підрядка буде видалено.Якщо підрядок не знайдено,
 //     функція повинна повертати оригінальний рядок.
 
+function removeSubstring(str, substr) {
+    const index = str.indexOf(substr);
+
+    if (index === -1) {
+        return str;
+    }
+    return str.slice(0, index) + str.slice(index + substr.length);
+}
 // Приклади використання:
 console.log(removeSubstring('Hello, world!', 'world')) // "Hello, !"
 console.log(removeSubstring('Hello, world!', 'JavaScript')) // "Hello, world!"
